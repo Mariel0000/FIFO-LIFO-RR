3f9f714dd59a02c8e59cd8c86c99b55b6aa35326
Mariel0000 <marielmodric@gmail.com>
2026-04-10 15:06:48 -0400

    mejora lifo

--- lifo.py
+++ lifo.py
@@ -4,3 +4,17 @@ def ejecutar_lifo(ti, t):
     done = [False] * n
     t_clock = 0
     completados = 0
+
+    while completados < n:
+        encontrado = False
+        for i in range(n-1, -1, -1):
+            if not done[i] and ti[i] <= t_clock:
+                t_clock += t[i]
+                tf[i] = t_clock
+                done[i] = True
+                completados += 1
+                encontrado = True
+                break
+        if not encontrado:
+            t_clock += 1
+    return tf
