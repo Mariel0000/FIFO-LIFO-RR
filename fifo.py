ce70b6ec7060735bb451588f93d0550347e0987a
Mariel0000 <marielmodric@gmail.com>
2026-04-10 11:36:48 -0400

    Add files via upload

---
+++ fifo.py
@@ -0,0 +1,20 @@
+def ejecutar_fifo(ti, t):
+    n = len(ti)
+    tf = [0] * n
+    done = [False] * n
+    t_clock = 0
+    completados = 0
+
+    while completados < n:
+        encontrado = False
+        for i in range(n):
+            if not done[i] and ti[i] <= t_clock:
+                t_clock += t[i]
+                tf[i] = t_clock
+                done[i] = True
+                completados += 1
+                encontrado = True
+                break
+        if not encontrado:
+            t_clock += 1
+    return tf
